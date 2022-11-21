4e7d5810a7815165acc1b0a4fb3116a3d0f6c2b8
silujnr <silujnr@gmail.com>
2022-11-22 00:25:11 +0530

    fixing

--- src/components/Welcome/ChairmanMessage/ChairmanMessage.js
+++ src/components/Welcome/ChairmanMessage/ChairmanMessage.js
@@ -13,25 +13,23 @@ const ChairmanMessage = () => {
                         {/* <!-- About Content Start --> */}
                         <div className="ChairmanMessage-content">
                             <h3 className="sec-title">
-                            President's Message
+                                President's Message
                             </h3>
                             <p className="sec-desc">
-                            Trauma Care  Kozhikode TRACK, having completed its 25 years of service and having been able to extend its support to tens of thousands of citizens in need, supported by selfless volunteers from different walks of life, from medical professionals, police officers and social leaders to common man, it is indeed my honour to have had this opportunity to be part of this unparalleled organization as one of its founding members and having had the privilege to lead a team of enthusiastic and socially committed individuals driven by passion and their sense of social responsibility
+                                Trauma Care  Kozhikode TRACK, having completed its 25 years of service and having been able to extend its support to tens of thousands of citizens in need, supported by selfless volunteers from different walks of life, from medical professionals, police officers and social leaders to common man, it is indeed my honour to have had this opportunity to be part of this unparalleled organization as one of its founding members and having had the privilege to lead a team of enthusiastic and socially committed individuals driven by passion and their sense of social responsibility
                             </p>
                             <p>As the society and its needs grow and the changing times demanding continuous revision of strategies and technology, I urge improved participation from all people – professionals, entrepreneurs, industrialists, young professionals, veteran specialists, students etc., in the services provided by TRACK, for us now and for a better tomorrow.</p>
                         </div>
-                        {/* <!-- About Content End --> */}
                     </div>
                     <div className="col-lg-4">
-                    {/* <Membershipbox /> */}
-                    <div class="team-member">
-    <div class="team-detail">
-    <img src={chimg} alt="" className="img-fluid mb-2"/>
-        <p class="designation">Commandent Retd.</p>
-        <h4><a href="/">Adv. Pradip Kumar C. M.</a></h4>
-        </div>
-        </div>
-                    
+                        <div classNameˀ="team-member">
+                            <div classNameˀ="team-detail">
+                                <img src={chimg} alt="" className="img-fluid mb-2" />
+                                <p classNameˀ="designation">Commandent Retd.</p>
+                                <h4><a href="/">Adv. Pradip Kumar C. M.</a></h4>
+                            </div>
+                        </div>
+
 
                     </div>
                 </div>
